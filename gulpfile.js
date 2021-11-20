3c683074bb475bdc1ad5925e1e4a97aee8d7370c
Dimeliora <dimeliora710@gmail.com>
2021-11-20 22:45:14 +0900

    packages update

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const del = require("del");
 const gulpIf = require("gulp-if");
 const rename = require("gulp-rename");
 const htmlMin = require("gulp-htmlmin");
-const sass = require("gulp-sass");
+const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
 const groupMediaQueries = require("gulp-group-css-media-queries");
 const autoprefixer = require("gulp-autoprefixer");
@@ -24,134 +24,134 @@ const webpackMode = isProd ? "production" : "development";
 
 // ======== HTML Handler ========
 const html = () =>
-	src(`${srcPath}/*.html`)
-		.pipe(
-			gulpIf(
-				isProd,
-				htmlMin({
-					collapseWhitespace: true,
-					removeComments: true,
-				})
-			)
-		)
-		.pipe(dest(destPath));
+  src(`${srcPath}/*.html`)
+    .pipe(
+      gulpIf(
+        isProd,
+        htmlMin({
+          collapseWhitespace: true,
+          removeComments: true,
+        })
+      )
+    )
+    .pipe(dest(destPath));
 
 // ======== SCSS Handler ========
 const scss = () =>
-	src(`${srcPath}/scss/styles.scss`, { allowEmpty: true })
-		.pipe(gulpIf(!isProd, sourcemaps.init()))
-		.pipe(
-			sass({
-				outputStyle: "expanded",
-			})
-		)
-		.pipe(groupMediaQueries())
-		.pipe(
-			autoprefixer({
-				cascade: true,
-				grid: true,
-			})
-		)
-		.pipe(
-			gulpIf(
-				isProd,
-				csso({
-					comments: false,
-				})
-			)
-		)
-		.pipe(gulpIf(!isProd, sourcemaps.write(".")))
-		.pipe(dest(destPath))
-		.pipe(browserSync.stream());
+  src(`${srcPath}/scss/styles.scss`, { allowEmpty: true })
+    .pipe(gulpIf(!isProd, sourcemaps.init()))
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(groupMediaQueries())
+    .pipe(
+      autoprefixer({
+        cascade: true,
+        grid: true,
+      })
+    )
+    .pipe(
+      gulpIf(
+        isProd,
+        csso({
+          comments: false,
+        })
+      )
+    )
+    .pipe(gulpIf(!isProd, sourcemaps.write(".")))
+    .pipe(dest(destPath))
+    .pipe(browserSync.stream());
 
 // ======== JS Handler ========
 const js = () =>
-	src(`${srcPath}/js/index.js`, { allowEmpty: true })
-		.pipe(
-			webpackStream({
-				mode: webpackMode,
-				output: {
-					filename: "script.js",
-				},
-				module: {
-					rules: [
-						{
-							test: /\.js$/,
-							exclude: /(node_modules)/,
-							use: "babel-loader",
-						},
-					],
-				},
-			})
-		)
-		.pipe(dest(destPath))
-		.pipe(browserSync.stream());
+  src(`${srcPath}/js/index.js`, { allowEmpty: true })
+    .pipe(
+      webpackStream({
+        mode: webpackMode,
+        output: {
+          filename: "script.js",
+        },
+        module: {
+          rules: [
+            {
+              test: /\.js$/,
+              exclude: /(node_modules)/,
+              use: "babel-loader",
+            },
+          ],
+        },
+      })
+    )
+    .pipe(dest(destPath))
+    .pipe(browserSync.stream());
 
 // ======== Images Handler ========
 const images = () =>
-	src(`${srcPath}/images/**/*.*`)
-		.pipe(rename({ dirname: "" }))
-		.pipe(newer(`${destPath}/images`))
-		.pipe(
-			imagemin([
-				imagemin.gifsicle({ interlaced: true }),
-				imagemin.mozjpeg({ quality: 75, progressive: true }),
-				imagemin.optipng({ optimizationLevel: 5 }),
-			])
-		)
-		.pipe(dest(`${destPath}/images`), {});
+  src(`${srcPath}/images/**/*.*`)
+    .pipe(rename({ dirname: "" }))
+    .pipe(newer(`${destPath}/images`))
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+      ])
+    )
+    .pipe(dest(`${destPath}/images`), {});
 
 // ======== WebP Converter ========
 const webpConvert = () =>
-	src(`${srcPath}/images/**/*.{png,jpg}`)
-		.pipe(rename({ dirname: "" }))
-		.pipe(newer(`${destPath}/images`))
-		.pipe(
-			webp({
-				quality: 70,
-			})
-		)
-		.pipe(dest(`${destPath}/images`));
+  src(`${srcPath}/images/**/*.{png,jpg}`)
+    .pipe(rename({ dirname: "" }))
+    .pipe(newer(`${destPath}/images`))
+    .pipe(
+      webp({
+        quality: 70,
+      })
+    )
+    .pipe(dest(`${destPath}/images`));
 
 // ======== SVG Icon Maker ========
 const svg = () =>
-	src(`${srcPath}/icons/**/*.svg`)
-		.pipe(
-			svgSprite({
-				mode: {
-					symbol: {
-						sprite: "../icon-sprite.svg",
-					},
-				},
-				shape: {
-					transform: [
-						{
-							svgo: {
-								plugins: [
-									{ removeViewBox: false },
-									{ cleanupIDs: false },
-									// { removeAttrs: { attrs: "(fill|stroke)" } },
-								],
-							},
-						},
-					],
-				},
-			})
-		)
-		.pipe(dest(`${destPath}/icons`));
+  src(`${srcPath}/icons/**/*.svg`)
+    .pipe(
+      svgSprite({
+        mode: {
+          symbol: {
+            sprite: "../icon-sprite.svg",
+          },
+        },
+        shape: {
+          transform: [
+            {
+              svgo: {
+                plugins: [
+                  { removeViewBox: false },
+                  { cleanupIDs: false },
+                  // { removeAttrs: { attrs: "(fill|stroke)" } },
+                ],
+              },
+            },
+          ],
+        },
+      })
+    )
+    .pipe(dest(`${destPath}/icons`));
 
 // ======== Fonts Copy Handler ========
 const fonts = () => src(`${srcPath}/fonts/*.*`).pipe(dest(`${destPath}/fonts`));
 
 // ======== Dev server ========
 const devServer = () => {
-	browserSync.init({
-		server: {
-			baseDir: destPath,
-		},
-		port: 8080,
-		notify: false,
-	});
+  browserSync.init({
+    server: {
+      baseDir: destPath,
+    },
+    port: 8080,
+    notify: false,
+  });
 };
 
 // ======== Dist Clear ========
@@ -159,17 +159,17 @@ const clear = () => del(destPath);
 
 // ======== Watchers ========
 const watchers = () => {
-	watch(`${srcPath}/*.html`, html).on("change", browserSync.reload);
-	watch(`${srcPath}/scss/**/*.scss`, scss);
-	watch(`${srcPath}/js/**/*.js`, js);
-	watch(`${srcPath}/images/**/*.*`, parallel(images, webpConvert));
-	watch(`${srcPath}/icons/**/*.svg`, svg);
+  watch(`${srcPath}/*.html`, html).on("change", browserSync.reload);
+  watch(`${srcPath}/scss/**/*.scss`, scss);
+  watch(`${srcPath}/js/**/*.js`, js);
+  watch(`${srcPath}/images/**/*.*`, parallel(images, webpConvert));
+  watch(`${srcPath}/icons/**/*.svg`, svg);
 };
 
 // ======== Build Task ========
 const build = series(
-	clear,
-	parallel(html, scss, js, images, webpConvert, svg, fonts)
+  clear,
+  parallel(html, scss, js, images, webpConvert, svg, fonts)
 );
 
 // ======== Build & Serve Task ========
